b69b6cd4ef4ba794c8362f6ca1315ea30971480e
Graeme Henderson <graeme.189@gmail.com>
2019-07-18 11:43:02 +0100

    Complete method to save student details & add seed data to database

---
+++ db/seeds.rb
@@ -0,0 +1,25 @@
+require('pry')
+
+require_relative('../models/student')
+
+HogwartsStudent.delete_all()
+
+student1 = HogwartsStudent.new({
+  "first_name" => "Harry",
+  "last_name" => "Potter",
+  "house" => "Gryffindor",
+  "age" => 11
+  })
+
+student2 = HogwartsStudent.new({
+  "first_name" => "Hermione",
+  "last_name" => "Grainger",
+  "house" => "Gryffindor",
+  "age" => 12
+  })
+
+student1.save
+student2.save
+
+binding.pry
+nil
